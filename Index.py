e8e3431247b1aa74ff19226678b3e02d6da7ecb2
ThiagoMedeiros21 <thfmadeiros01@gmail.com>
2024-12-04 17:38:38 -0300

    criação de função e menu nao funcional

--- Index.py
+++ Index.py
@@ -1,4 +1,17 @@
 import os 
 os.system('cls')
 
-print('Desenvolvimento em Breve...')
+def criar_contatos(contato,numero):
+    try:
+        with open('Agenda_contatos.txt','a') as file:
+            file.write(f'{contato}, {numero}')
+    except FileNotFoundError:
+        print('O Arquivo não foi Encontrado')
+
+print(''''BEM VINDO A SUA AGENDA DE CONTATOS:
+ DIGITE:
+ 1-Adicionar um Contato
+ 2-Ler um Contato
+ 3-Deletar um Contato
+ 4-Atualizar um Contato
+''')
