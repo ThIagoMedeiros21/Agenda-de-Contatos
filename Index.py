5b3ce4693ada9efd2206d2799b9850b6d3045b07
ThiagoMedeiros21 <thfmadeiros01@gmail.com>
2024-12-14 12:37:30 -0300

    Crud com todas as funções ja criadas e funcionais

--- Index.py
+++ Index.py
@@ -15,12 +15,27 @@ def ler_contatos():
     except FileNotFoundError:
         print('Arquivo não encontrado')
 
-def edit_contato ():
+def edit_contato(contato, contatonovo, novonum):
     try:
-        ...
+        with open("Agenda_contatos.txt", 'r') as file:
+            linhas=file.readlines()
+        encontrado=False
+        with open("Agenda_contatos.txt", 'w') as file:
+            for c in linhas:
+                if not c.startswith(f'{contato}:'):
+                    file.write(c)
+                else:
+                    encontrado=True
+        if encontrado:
+            with open("Agenda_contatos.txt", 'a') as file:
+                file.write(f'{contatonovo}: {novonum}\n')
+            os.system("cls")
+            print(f"Contato '{contato}' atualizado para '{contatonovo}: {novonum}'")
+        else:
+            os.system("cls")
+            print(f"Contato '{contato}' não encontrado.")
     except FileNotFoundError:
-        print('O Arquivo ainda não foi criado')
-
+        print('O Arquivo ainda não foi criado.')
 def apagar_arquivo():
     arquivo=('Agenda_contatos.txt')
     if os.path.exists(arquivo):
@@ -33,15 +48,15 @@ def apagar_arquivo():
 
 def deletar_contato(contato):
     try:
-        with open('Agenda_contatos.txt', 'r') as file:
-            linhas = file.readlines()
-        encontrado = False
-        with open('Agenda_contatos.txt', 'w') as file:
+        with open('Agenda_contatos.txt','r') as file:
+            linhas=file.readlines()
+        encontrado=False
+        with open('Agenda_contatos.txt','w') as file:
             for i in linhas:
                 if not i.startswith(f'{contato}:'):
                     file.write(i)
                 else:
-                    encontrado = True
+                    encontrado=True
         if encontrado:
             os.system("cls")
             print(f'O contato "{contato}" foi removido com sucesso.')
@@ -80,6 +95,7 @@ while True:
     Digite:
     1-Deletar toda a Agenda
     2-Deletar um Contato específico 
+    3-SAIR
     '''))
             if confirmar==1:
                 fim=input("DESEJA CANCELAR TODOS OS CONTATOS? (S/n): ").lower()
@@ -98,9 +114,22 @@ while True:
                 contato=input("Digite o nome do contato a ser removido: ")
                 deletar_contato(contato)
                 break
+            elif(confirmar==3):
+                os.system("cls")
+                break
             else:
                 os.system('cls')
                 print('Digite novamente:')
                 continue
+    elif option==4:
+        os.system("cls")
+        contato=input("Digite o nome do contato que deseja alterar: ")
+        novo_contato=input("Digite o nome corretamente ")
+        numero=input("digite o novo numero")
+        if len(numero)==8:
+            edit_contato(contato,novo_contato,numero)
+        else:
+            print("Digite novamente")
+
     elif option==5:
         break
